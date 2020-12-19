d76cb877f60173ab6afbd75e94c6c5d76a3ccfb5
MaxiDeMichieli <maxidemichieli@gmail.com>
2020-12-19 19:02:49 -0300

    Add theme configuration

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#091762',
+    },
+    secondary: {
+      main: '#404040',
+    },
+  },
+});
+
+export default theme;
