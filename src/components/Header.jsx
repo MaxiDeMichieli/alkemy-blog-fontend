1d6e7fb8de4c63adc9308a803e9b5277bc61be3c
MaxiDeMichieli <maxidemichieli@gmail.com>
2020-12-19 19:57:10 -0300

    Modify header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import {
-  AppBar, Toolbar, Button, Container,
+  AppBar, Toolbar, Button, Container, Box,
 } from '@material-ui/core';
 import HomeIcon from '@material-ui/icons/Home';
 import SettingsIcon from '@material-ui/icons/Settings';
@@ -12,38 +12,43 @@ const useStyles = makeStyles(() => ({
   logo: {
     flexGrow: 1,
   },
-  button: {
-    margin: '0 10px',
-  },
 }));
 
 function ButtonAppBar() {
   const classes = useStyles();
 
   return (
-    <AppBar position="fixed">
-      <Container>
+    <AppBar position="sticky">
+      <Container maxWidth="md">
         <Toolbar>
           <div className={classes.logo}>
             <Link to="/">
               <img src={logo} alt="logo-alk" />
             </Link>
           </div>
-          <Button
-            variant="contained"
-            color="primary"
-            className={classes.button}
-            startIcon={<HomeIcon />}
-          >
-            home
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            startIcon={<SettingsIcon />}
-          >
-            admin
-          </Button>
+          <Link to="/">
+            <Box mr={1}>
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                startIcon={<HomeIcon />}
+              >
+                home
+              </Button>
+            </Box>
+          </Link>
+          <Link to="/admin">
+            <Box>
+              <Button
+                variant="contained"
+                color="primary"
+                startIcon={<SettingsIcon />}
+              >
+                admin
+              </Button>
+            </Box>
+          </Link>
         </Toolbar>
       </Container>
     </AppBar>
