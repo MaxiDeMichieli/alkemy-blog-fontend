04439e137111f8ea86587202218be69c2c0b1c92
MaxiDeMichieli <maxidemichieli@gmail.com>
2020-12-19 15:37:34 -0300

    Create structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Router from './router/Router';
+
+function App() {
+  return (
+    <>
+      <header>
+        Header
+      </header>
+      <Router />
+    </>
+  );
+}
+
+export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/jsx-filename-extension */
+
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
