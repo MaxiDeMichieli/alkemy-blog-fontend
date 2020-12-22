bda9e5043bd288b59d9cd98a0be46d8fc2c8dc9e
MaxiDeMichieli <maxidemichieli@gmail.com>
2020-12-22 11:17:10 -0300

    Delete env in axios config

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: 'https://challenge-alkemy-blog.herokuapp.com/api',
 });
 
 export default http;
